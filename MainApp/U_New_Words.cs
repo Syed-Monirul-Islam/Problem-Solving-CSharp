2a0f4c2ff1df3e164b32837463c002abc692f167
Moni <syedmonirulislamcu@gmail.com>
2026-06-09 00:13:56 +0600

    Solve Codeforces U. New Words

---
+++ MainApp/U_New_Words.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+    internal class U_New_Words
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine().ToLower();
+
+            int e = 0, g = 0, y = 0, p = 0, t = 0;
+
+            foreach (char ch in s)
+            {
+                if (ch == 'e') e++;
+                else if (ch == 'g') g++;
+                else if (ch == 'y') y++;
+                else if (ch == 'p') p++;
+                else if (ch == 't') t++;
+            }
+
+            int[] counts = { e, g, y, p, t };
+            int result = counts.Min();
+
+            Console.WriteLine(result);
+        }
+    }
+}
