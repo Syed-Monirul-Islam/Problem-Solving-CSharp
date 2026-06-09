3026d91859d747a7b8ef8f22beb58cfcdf089044
Moni <syedmonirulislamcu@gmail.com>
2026-06-09 17:46:08 +0600

    Add solution for Codeforces Palindrome problem

---
+++ I_Palindrome/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace I_Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string reversed = new string(s.Reverse().ToArray());
+
+            if (s == reversed)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
